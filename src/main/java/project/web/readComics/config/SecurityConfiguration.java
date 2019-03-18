045bc9d8d6756fb10f18a01a10d402c4e947a8c7
AleksandarDi <aleksandar.dimitriev@students.finki.ukim.mk>
2019-03-18 02:15:33 +0100

    Revert "m"
    This reverts commit 1db59ae

--- src/main/java/project/web/readComics/config/SecurityConfiguration.java
+++ src/main/java/project/web/readComics/config/SecurityConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,13 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import project.web.readComics.repository.UsersRepository;
 import project.web.readComics.service.VideoUserDetailsService;
 
@@ -52,14 +45,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS, "http://localhost:8080/sign_up")
-                .authenticated()
-                .antMatchers(HttpMethod.OPTIONS, "http://localhost:3000/login").authenticated()
+                .antMatchers("**/sign_up","**/").permitAll()
                 .anyRequest().permitAll()
                 .and()
                 .formLogin().loginPage("http://localhost:3000/login").loginProcessingUrl("/login").permitAll()
                 .defaultSuccessUrl("http://localhost:3000/home",true);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-
 }
